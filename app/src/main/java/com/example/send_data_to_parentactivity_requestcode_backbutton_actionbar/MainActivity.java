ebfc8de22ca79ac8e97585d204a142b9d8e507af
Aman Sharma <amansharma.11@hotmail.com>
2019-07-12 17:36:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/send_data_to_parentactivity_requestcode_backbutton_actionbar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.send_data_to_parentactivity_requestcode_backbutton_actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
